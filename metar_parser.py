74abfe8c0cfdbc0b6bfb56bb698bd1e05decae1e
Creasure <yurxo@bk.ru>
2026-05-28 03:12:15 +0300

    parser: allow [DNU] after both RVRs

--- metar_parser.py
+++ metar_parser.py
@@ -28,7 +28,7 @@ metar_regex = re.compile(r"^" +
     # [12?] => visibility string
     r"(?:\s+([\d\/]{4}(?:\s+\d{4}(?:N|NE|E|SE|S|SW|W|NW))*|(?:\d{1,2}\s+)?\d{1,2}(?:\/[24])?SM))?" +
     # [13?] => RVR string
-    r"((?:\s+R\d{1,2}[LCR]?\/[MP]?\d{4}(?:V[MP]?\d{4})?(?:[DNU]|FT)?)*)" +
+    r"((?:\s+R\d{1,2}[LCR]?\/[MP]?\d{4}[DNU]?(?:V[MP]?\d{4}[DNU]?)?(?:FT)?)*)" +
     # [14?] => weather string
     weather_regex +
     # [15?] => cloud string
